ced5abfe8ef6f895c4b02895a751c78f47aa1017
samuguiro <samuel.guinaldo@outlook.com>
2016-08-13 18:12:00 +0200

    Prueba1

---
+++ app/src/main/java/com/acme/proyecto/facturacion/Parametros.java
@@ -0,0 +1,12 @@
+package com.acme.proyecto.facturacion;
+
+/**
+ * Created by guinaldo on 13/08/2016.
+ */
+public class Parametros {
+
+    static String alesur = "Alesur S.A.";
+    static String palmacargo = "Palmacargo S.A.";
+
+
+}
